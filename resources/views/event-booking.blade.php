132a18a4caf2d36d8f6fef11208f335fb7f410a8
satishccy <satishchoudhari13579@gmail.com>
2024-11-26 11:45:55 +0530

    update iframe url

--- resources/views/event-booking.blade.php
+++ resources/views/event-booking.blade.php
@@ -8,6 +8,6 @@
 
 @section('content')
 <div style="margin-top:100px;margin-bottom:60px;display:flex;flex-direction:row;justify-content:center;">
-    <iframe id="iframeticket" allowtransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://www.tickets99.com/e/qa-summit-bengaluru/buy/qa-summit-bengaluru/ticket" frameborder="0" width="90%" height="100%" scrolling="auto"> </iframe>
+    <iframe id="iframeticket" allowtransparency="true" allowfullscreen="true" allow="geolocation; microphone; camera" src="https://www.tickets99.com/buy/qa-summit-bengaluru/ticket" frameborder="0" width="90%" height="100%" scrolling="auto"> </iframe>
 </div>
 @endsection
